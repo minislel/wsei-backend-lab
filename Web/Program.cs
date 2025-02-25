76947ea7dd254a0d78e8ecee302d7aef832f8323
minislel <marcin4kt@gmail.com>
2025-02-25 15:43:15 +0100

    Add project files.

---
+++ Web/Program.cs
@@ -0,0 +1,31 @@
+using ApplicationCore.Interfaces.Repository;
+using BackendLab01;
+using Infrastructure.Memory;
+using Infrastructure.Memory.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddRazorPages();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.Seed();
+app.Run();
---
+++ Web/SeedData.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Interfaces.Repository;
+using BackendLab01;
+
+namespace Infrastructure.Memory;
+public static class SeedData
+{
+    public static void Seed(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var provider = scope.ServiceProvider;
+            var quizRepo = provider.GetService<IGenericRepository<Quiz, int>>();
+            var quizItemRepo = provider.GetService<IGenericRepository<QuizItem, int>>();
+        }
+    }
+}
