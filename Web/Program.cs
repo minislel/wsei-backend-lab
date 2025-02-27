0069e2705a99a906a8d185d6f58dac5aed28677f
minislel <marcin4kt@gmail.com>
2025-02-27 17:17:59 +0100

    implemented transients and singletones

--- Web/Program.cs
+++ Web/Program.cs
@@ -7,6 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<IGenericGenerator<int>,IntGenerator>();
+builder.Services.AddSingleton<IGenericRepository<Quiz,int>, MemoryGenericRepository<Quiz, int>>();
+builder.Services.AddSingleton<IGenericRepository<QuizItem, int>, MemoryGenericRepository<QuizItem, int>>();
+builder.Services.AddSingleton<IGenericRepository<QuizItemUserAnswer, string>, MemoryGenericRepository<QuizItemUserAnswer, string>>();
+builder.Services.AddSingleton<IQuizUserService, QuizUserService>();
+builder.Services.AddSingleton<IQuizAdminService, QuizAdminService>();
+
 
 
 var app = builder.Build();
--- Web/SeedData.cs
+++ Web/SeedData.cs
@@ -1,5 +1,6 @@
 ﻿using ApplicationCore.Interfaces.Repository;
 using BackendLab01;
+using System.Drawing;
 
 namespace Infrastructure.Memory;
 public static class SeedData
@@ -11,6 +12,18 @@ public static class SeedData
             var provider = scope.ServiceProvider;
             var quizRepo = provider.GetService<IGenericRepository<Quiz, int>>();
             var quizItemRepo = provider.GetService<IGenericRepository<QuizItem, int>>();
-        }
+			var quizItemUserAnswerRepo = provider.GetService<IGenericRepository<QuizItemUserAnswer, string>>();
+            var AdminService = provider.GetService<IQuizAdminService>();
+            quizItemRepo.Add(new QuizItem(0, "Pierwsza litera alfabetu?", new List<string>() { "B", "C", "D" }, "A"));
+            quizItemRepo.Add(new QuizItem(0, "Druga litera alfabetu?", new List<string>() { "A", "C", "E" }, "B"));
+			quizItemRepo.Add(new QuizItem(0, "Trzecia litera alfabetu?", new List<string>() { "A", "B", "F" }, "C"));
+			quizItemRepo.Add(new QuizItem(0, "Czwarta litera alfabetu?", new List<string>() { "A", "B", "F" }, "D"));
+			quizItemRepo.Add(new QuizItem(0, "Piąta litera alfabetu?", new List<string>() { "A", "B", "F" }, "E"));
+			quizItemRepo.Add(new QuizItem(0, "Szósta litera alfabetu?", new List<string>() { "A", "B", "F" }, "F"));
+
+
+
+			AdminService.AddQuiz("tesotwy 1", AdminService.FindAllQuizItems());
+		}
     }
 }
